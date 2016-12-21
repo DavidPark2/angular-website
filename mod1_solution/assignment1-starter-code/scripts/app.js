b969525dc0ec4785d45e5734f952f72f943f4094
david <dpark543107@gmail.com>
2016-12-21 17:28:15 -0600

    question 2 starter code

--- mod1_solution/assignment1-starter-code/scripts/app.js
+++ mod1_solution/assignment1-starter-code/scripts/app.js
@@ -20,7 +20,7 @@ function LunchCheckController($scope) {
   $scope.cssStyling = ""
 
   $scope.fullnessMessage = function () {
-    var fullnessMeasure = convertStringToArray($scope.lunchList)
+    var fullnessMeasure = convertStringToArray(testingSuff(testingss($scope.lunchList)))
     var stylingTheCss = colorAndBorder(fullnessMeasure)
 
     $scope.lunchMessage = fullnessMeasure
@@ -46,5 +46,43 @@ function LunchCheckController($scope) {
       return "hasData"
     }
   }
+
+  var testing = "hey , , , hey , , hey, , , ,"
+  var testingss = function(data) {
+    data = data.split('')
+    var newArray = [];
+
+    for (var i = 0; i < data.length; i++) {
+      if (data[i] === ' ') {
+        newArray.push('')
+      } else {
+        newArray.push(data[i])
+      }
+    }
+
+    return newArray.join('')
+  }
+
+  var testingSuff = function(data) {
+    var correctShit = []
+
+    for (var i = 0; i < data.length; i++) {
+      if (i === 0) {
+        correctShit.push(data[i])
+      } else if (i === data.length - 1) {
+
+      } else if (data[i] === ',' && data[i - 1] === ',') {
+        correctShit.push('')
+      } else if (data[i] === ',') {
+        correctShit.push(', ')
+      } else {
+        correctShit.push(data[i])
+      }
+    }
+
+    return correctShit.join('').split(' ')
+  }
+  console.log(testingss(testing))
+  console.log(testingSuff(testingss(testing)))
 }
 })()

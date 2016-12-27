e05517a9930fcf0f0cbdbf474ce13236d8a56327
david <dpark543107@gmail.com>
2016-12-27 17:29:03 -0600

    created menuSearchService service

--- mod3_solution/assignment3-starter-code/app.js
+++ mod3_solution/assignment3-starter-code/app.js
@@ -2,5 +2,27 @@
 'use strict'
 
 angular.module('NarrowItDownApp', [])
-  
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+
+NarrowItDownController.$inject = ['MenuSearchService']
+function NarrowItDownController(MenuSearchService) {
+  var promise = MenuSearchService.getMatchedMenuItems();
+
+  console.log(promise)
+}
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath']
+function MenuSearchService($http, ApiBasePath) {
+  var service = this
+
+  service.getMatchedMenuItems = function(searchTerm) {
+    var response = $http({
+      method: 'GET',
+      url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+    })
+
+    return response
+  }
+}
 })()
